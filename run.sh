68df3c0f2f0804a6cff1eddff6a4bf9f1b339a78
Brian Dentino <brian.dentino@gmail.com>
2016-09-12 23:55:27 -0700

    read datasources/dashboards from all services, add grafana.ini conf template

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-while [[ ! -e /etc/rancher-conf/dashboards ]]; do
-  echo "Waiting for grafana dashboards dir..."
+while [[ ! -e /etc/rancher-conf/grafana.ini ]]; do
+  echo "Waiting for grafana config..."
   sleep 2
 done
+
+cp /etc/rancher-conf/grafana.ini /etc/grafana/grafana.ini
+chown -R grafana:grafana /etc/grafana/grafana.ini
+
+exec /run.sh
